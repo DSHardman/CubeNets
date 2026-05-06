2cbf0bb472f7e60a048e02b429112aa3ebaee6b8
David <dshardman@outlook.com>
2026-05-06 12:59:48 +0100

    cube folding and initial figure drafting

---
+++ ManualMeasurements.m
@@ -0,0 +1,96 @@
+%% Part 1: measurement ratios
+y = [17.6 14.9 0.5; 13.3 14.9 0.14];
+bar(categorical({'A', 'B'}), y,'stacked');
+colororder("earth")
+set(gca, "linewidth", 2, "Fontsize", 15);
+box off
+set(gcf, 'color', 'w');
+ylabel("Resistance (k\Omega)");
+legend({"X_1 Connection"; "X_2 Connection"; "Surface Measurement"});
+legend boxoff
+
+return
+
+%% Part 2: spatial distribution
+
+circles = [1, 0, 0.14;
+    0 1 0.17;
+    1 1 0.19;
+    0 2 0.25;
+    1 2 0.25;
+    0 3 0.3;
+    1 3 0.29;
+    -2 4 0.52;
+    -1 4 0.42;
+    0 4 0.37;
+    1 4 0.35;
+    2 4 0.40;
+    3 4 0.46;
+    -2 5 0.53;
+    -1 5 0.45;
+    0 5 0.41;
+    1 5 0.41;
+    2 5 0.41;
+    3 5 0.48;
+    0 6 0.47;
+    1 6 0.47;
+    0 7 0.51;
+    1 7 0.50];
+
+scatter(circles(:, 1), circles(:,2), 500*circles(:,3), 'k', 'filled');
+hold on
+scatter(0, 0, 100, 'kx', 'linewidth', 2);
+
+for i = 1:size(circles, 1)
+    text(circles(i, 1), circles(i, 2)-0.35, string(circles(i,3)),...
+        "HorizontalAlignment", "center", "Color", "k");
+end
+
+axis equal
+axis off
+set(gcf, 'color', 'w', 'position', [1     1   868   719]);
+
+plot(polyshape([-0.5 1.5 1.5 3.5 3.5 1.5 1.5 -0.5 -0.5 -2.5 -2.5 -0.5 -0.5],...
+    [-0.5 -0.5 3.5 3.5 5.5 5.5 7.5 7.5 5.5 5.5, 3.5 3.5 -0.5]), 'facecolor', 'none');
+
+%% Part 3: spatial distribution with ribbons
+
+circles = [1, 0, 28;
+    0 1 30;
+    1 1 26;
+    0 2 32;
+    1 2 28;
+    0 3 34;
+    1 3 30;
+    -2 4 38;
+    -1 4 39;
+    0 4 35;
+    1 4 35;
+    2 4 26;
+    3 4 24;
+    -2 5 34;
+    -1 5 31;
+    0 5 29;
+    1 5 27;
+    2 5 25;
+    3 5 23;
+    0 6 30;
+    1 6 28;
+    0 7 30;
+    1 7 32];
+
+scatter(circles(:, 1), circles(:,2), 10*circles(:,3), 'k', 'filled');
+hold on
+scatter(0, 0, 100, 'kx', 'linewidth', 2);
+
+for i = 1:size(circles, 1)
+    text(circles(i, 1), circles(i, 2), string(circles(i,3)),...
+        "HorizontalAlignment", "center", "Color", "red");
+end
+
+axis equal
+axis off
+set(gcf, 'color', 'w');
+
+plot(polyshape([-0.5 1.5 1.5 3.5 3.5 1.5 1.5 -0.5 -0.5 -2.5 -2.5 -0.5 -0.5],...
+    [-0.5 -0.5 3.5 3.5 5.5 5.5 7.5 7.5 5.5 5.5, 3.5 3.5 -0.5]), 'facecolor', 'none');
---
+++ foldcube.m
@@ -0,0 +1,81 @@
+function foldcube(responseobject, response, visiblesides, multiplier)
+    
+    clear sides
+    for i = 1:6
+        sides(i) = SkinResponse([], NaN, []);
+    end
+
+    % Sort into cube sides
+    for i = 1:length(responseobject.positions(:, 1))
+        if responseobject.positions(i, 1) < 30
+            % SIDE 1
+            sides(1).positions = [sides(1).positions; responseobject.positions(i, :)];
+            sides(1).responses = [sides(1).responses; responseobject.responses(i, :)];
+        elseif responseobject.positions(i, 1) > 60 && responseobject.positions(i, 1) < 90
+            % SIDE 3
+            sides(3).positions = [sides(3).positions; responseobject.positions(i, :)];
+            sides(3).responses = [sides(3).responses; responseobject.responses(i, :)];
+        elseif responseobject.positions(i, 1) > 90
+            % SIDE 4
+            sides(4).positions = [sides(4).positions; responseobject.positions(i, :)];
+            sides(4).responses = [sides(4).responses; responseobject.responses(i, :)];
+        elseif responseobject.positions(i, 2) > 30
+            % SIDE 5
+            sides(5).positions = [sides(5).positions; responseobject.positions(i, :)];
+            sides(5).responses = [sides(5).responses; responseobject.responses(i, :)];
+        elseif responseobject.positions(i, 2) < 0
+            % SIDE 6
+            sides(6).positions = [sides(6).positions; responseobject.positions(i, :)];
+            sides(6).responses = [sides(6).responses; responseobject.responses(i, :)];
+        else
+            % SIDE 2
+            sides(2).positions = [sides(2).positions; responseobject.positions(i, :)];
+            sides(2).responses = [sides(2).responses; responseobject.responses(i, :)];
+        end
+    end
+
+    plot3(nan, nan, nan);
+    hold on
+
+    if visiblesides(1)
+        scatter3(zeros(size(sides(1).positions(:,1))), sides(1).positions(:,2)./30, sides(1).positions(:,1)./30, 80, sides(1).responses(:, response), 'filled');
+    end
+
+    if visiblesides(2)
+        scatter3((sides(2).positions(:,1)-30)./30, sides(2).positions(:,2)./30, ones(size(sides(2).positions(:,1))), 80, sides(2).responses(:, response), 'filled');
+    end
+
+    if visiblesides(3)
+        scatter3(ones(size(sides(3).positions(:,1))), sides(3).positions(:,2)./30, (90-sides(3).positions(:,1))./30, 80, sides(3).responses(:, response), 'filled');
+    end
+
+    if visiblesides(4)
+        scatter3((120-sides(4).positions(:,1))./30, sides(4).positions(:,2)./30, zeros(size(sides(4).positions(:,1))), 80, sides(4).responses(:, response), 'filled');
+    end
+
+    if visiblesides(5)
+        scatter3((sides(5).positions(:,1)-30)./30, ones(size(sides(5).positions(:,1))), (60-sides(5).positions(:,2))./30, 80, sides(5).responses(:, response), 'filled');
+    end
+
+    if visiblesides(6)
+        scatter3((sides(6).positions(:,1)-30)./30, zeros(size(sides(6).positions(:,1))), (sides(6).positions(:,2)+30)./30, 80, sides(6).responses(:, response), 'filled');
+    end
+
+    line([0 0], [0 0], [0 1], 'Color', 'k');
+    line([0 0], [0 1], [0 0], 'Color', 'k');
+    line([0 1], [0 0], [0 0], 'Color', 'k');
+    line([0 0], [0 1], [1 1], 'Color', 'k');
+    line([0 1], [0 0], [1 1], 'Color', 'k');
+    line([1 1], [1 0], [0 0], 'Color', 'k');
+    line([1 1], [1 1], [0 1], 'Color', 'k');
+    line([1 0], [1 1], [0 0], 'Color', 'k');
+    line([1 1], [0 1], [1 1], 'Color', 'k');
+    line([1 1], [0 0], [1 0], 'Color', 'k');
+    line([0 1], [1 1], [1 1], 'Color', 'k');
+    line([0 0], [1 1], [1 0], 'Color', 'k');
+
+    axis equal
+    axis off
+    set(gcf, 'color', 'w')
+    view(3)
+end
